e801cc691ec9ec1485f2970afc674f15f0284352
Jiongyi Tan <jiongyi.tan@ucsf.edu>
2019-03-11 11:25:46 -0700

    cleaned up bead motility code

--- actin_assembly/bead_motility/bead_motility.py
+++ actin_assembly/bead_motility/bead_motility.py
@@ -24,29 +24,43 @@ def find_tif_files(folder_name_str):
                 tif_file_path_list.append(join(dir_name_str, i_file_name))
     return tif_file_path_list
 
-def tophat_reconstruction(raw_mat, elem_mat):
-    eroded_mat = erosion(raw_mat, elem_mat)
-    opened_mat = reconstruction(eroded_mat, raw_mat)
-    tophat_mat = raw_mat - opened_mat
-    return tophat_mat
-
-def binarize_npf(file_path_str, save_image = False):
+def make_rgb_overlay(gray_mat, bw_mat, rgb_color_row):
+    max_type = dtype_limits(gray_mat)[1]
+    red_scale = rgb_color_row[0]
+    green_scale = rgb_color_row[1]
+    blue_scale = rgb_color_row[2]
+    rgb_mat = stack((gray_mat, gray_mat, gray_mat), axis = -1)
+    rgb_mat[bw_mat, 0] = red_scale * max_type
+    rgb_mat[bw_mat, 1] = green_scale * max_type
+    rgb_mat[bw_mat, 2] = blue_scale * max_type
+    return rgb_mat
+    
+def measure_actin_density(file_path_str, save_images = False):
+    # Load micromanager stack.
     mm_stack = imread(file_path_str)
+    # Binarize npf-coated bead surface using Canny edge detector.
     npf_mat = mm_stack[0, :, :]
-    rescaled_mat = rescale_intensity(npf_mat)
-    bw_mat = binary_fill_holes(canny(rescaled_mat, sigma = 0.3))
+    npf_rescaled_mat = rescale_intensity(npf_mat)
+    bw_mat = binary_fill_holes(canny(npf_rescaled_mat, sigma = 0.3))
     bw_mat = remove_small_objects(bw_mat, min_size = sum(disk(6)))
     bw_mat ^= erosion(bw_mat, disk(3))
+    # Measure npf intensity.
     label_mat, no_labels = label(bw_mat, return_num = True)
     mean_background_int = mean(npf_mat[~bw_mat])
     properties_list = regionprops(label_mat, npf_mat - mean_background_int)
     diameter_row = 0.5 * MICRON_PER_PIXEL * array([x.major_axis_length + x.minor_axis_length for x in properties_list])
     is_large_row = diameter_row > DIAMETER_THRESHOLD
     npf_intensity_row = array([x.mean_intensity for x in properties_list])
-    actin_intensity_row = zeros(npf_intensity_row.shape)
+    # Save images.
+    if save_images == True:
+        npf_rgb_mat = make_rgb_overlay(npf_rescaled_mat, bw_mat, [0.0, 1.0, 0.0])
+        imsave(file_path_str[:-4] + '_npf_segmentation.tif', img_as_uint(npf_rgb_mat))
     
+    # Measure actin intensity.
+    actin_intensity_row = zeros(npf_intensity_row.shape)
     if no_labels >= 1:
         actin_mat = gaussian(mm_stack[1, :, :], sigma = 0.5)
+        actin_rescaled_mat = rescale_intensity(mm_stack[1, :, :])
         mean_actin_back_au = mean(actin_mat)
         std_actin_back_au = std(actin_mat)
         bw_otsu_mat = actin_mat > max(threshold_otsu(actin_mat),
@@ -63,25 +77,18 @@ def binarize_npf(file_path_str, save_image = False):
             else:
                 actin_intensity_row[i - 1] = mean(actin_mat[i_bw_actin_mat])
                 bw_actin_mat[i_bw_search_mat & bw_otsu_mat] = True
-        #imsave(file_path_str[:-4] + '_reconstructed.tif', img_as_uint(bw_actin_mat))
-        
-    # Save images.
-    if save_image == True:
-        zeros_mat = copy(rescaled_mat)
-        zeros_mat[bw_mat] = dtype_limits(npf_mat)[0]
-        ones_mat = copy(rescaled_mat)
-        ones_mat[bw_mat] = dtype_limits(npf_mat)[1]
-        rgb_mat = stack((zeros_mat, ones_mat, zeros_mat), axis = -1)
-        imsave(file_path_str[:-4] + '_segmentation.tif', img_as_uint(rgb_mat))
+        if save_images == True:
+            actin_rgb_mat = make_rgb_overlay(actin_rescaled_mat, bw_actin_mat, [1.0, 0.0, 0.0])
+            imsave(file_path_str[:-4] + '_actin_segmentation.tif', img_as_uint(actin_rgb_mat))
     return npf_intensity_row, actin_intensity_row
         
-def measure_npf_density(folder_name_str):
+def batch_measure_actin_density(folder_name_str, save_images = False):
     file_path_list = find_tif_files(folder_name_str)
     no_files = len(file_path_list)
     npf_intensity_row = array([])
     actin_intensity_row = array([])
     for i in range(no_files):
-        i_intensity_row, i_actin_intensity_row = binarize_npf(file_path_list[i])
+        i_intensity_row, i_actin_intensity_row = measure_actin_density(file_path_list[i], save_images)
         npf_intensity_row = append(npf_intensity_row, i_intensity_row)
         actin_intensity_row = append(actin_intensity_row, i_actin_intensity_row)
     return npf_intensity_row, actin_intensity_row
