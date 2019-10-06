a6e7fdeda718acad9ee304b045382096fffac561
Jiongyi Tan <jiongyi.tan@ucsf.edu>
2019-10-06 15:28:45 -0700

    Worked on bead motility code

--- actin_assembly/bead_motility/bead_motility.py
+++ actin_assembly/bead_motility/bead_motility.py
@@ -1,16 +1,15 @@
 from os import walk
 from os.path import join, split
 import fnmatch
-from skimage import img_as_uint, dtype_limits
+from skimage import img_as_float, img_as_uint, dtype_limits
 from skimage.io import imread, imsave
 from skimage.feature import canny
 from scipy.ndimage.morphology import binary_fill_holes
-from skimage.morphology import disk, dilation, erosion, remove_small_objects, reconstruction
+from skimage.morphology import disk, dilation, erosion, remove_small_objects, reconstruction, medial_axis, thin
 from skimage.filters import threshold_otsu, gaussian
-from numpy import sum, mean, std, array, copy, stack, append, zeros, all
+from numpy import sum, mean, std, array, empty, vstack, zeros, stack
 from skimage.measure import label, regionprops
 from skimage.segmentation import find_boundaries
-from skimage.exposure import rescale_intensity
 
 MICRON_PER_PIXEL = 0.16
 DIAMETER_THRESHOLD = 2.0
@@ -24,102 +23,92 @@ def find_tif_files(folder_name_str):
                 tif_file_path_list.append(join(dir_name_str, i_file_name))
     return tif_file_path_list
 
-def make_rgb_overlay(gray_mat, bw_mat, rgb_color_row):
-    max_type = dtype_limits(gray_mat)[1]
+def read_and_split(file_path_str):
+    mm_stack = imread(file_path_str)
+    npf_im = img_as_float(mm_stack[0, :, :])
+    actin_im = img_as_float(mm_stack[1, :, :])
+    return npf_im, actin_im
+
+def binarize_npf(npf_im):
+    npf_rescaled_im = npf_im / max(npf_im.flatten())
+    bw_im = binary_fill_holes(canny(npf_rescaled_im, sigma = 0.1))
+    bw_im = remove_small_objects(bw_im, min_size = sum(disk(6)))
+    npf_bw_im = bw_im ^ erosion(bw_im, disk(3))
+    return npf_bw_im
+
+def measure_npf_fluor(npf_im, npf_bw_im):
+    label_im, no_labels = label(npf_bw_im, return_num = True)
+    mean_background_fluor = mean(npf_bw_im[~npf_bw_im])
+    properties_list = regionprops(label_im, npf_im - mean_background_fluor)
+    npf_fluor_row = array([x.mean_intensity for x in properties_list]) * (2**16 - 1)
+    return npf_fluor_row
+
+def make_rgb_overlay(gray_im, bw_im, rgb_color_row):
+    gray_im /= max(gray_im.flatten())
+    max_type = dtype_limits(gray_im)[1]
     red_scale = rgb_color_row[0]
     green_scale = rgb_color_row[1]
     blue_scale = rgb_color_row[2]
-    rgb_mat = stack((gray_mat, gray_mat, gray_mat), axis = -1)
-    rgb_mat[bw_mat, 0] = red_scale * max_type
-    rgb_mat[bw_mat, 1] = green_scale * max_type
-    rgb_mat[bw_mat, 2] = blue_scale * max_type
-    return rgb_mat
+    rgb_im = stack((gray_im, gray_im, gray_im), axis = -1)
+    rgb_im[bw_im, 0] = red_scale * max_type
+    rgb_im[bw_im, 1] = green_scale * max_type
+    rgb_im[bw_im, 2] = blue_scale * max_type
+    return rgb_im
 
-def measure_npf_density(file_path_str, save_images = False):
-    # Load micromanager stack.
-    mm_stack = imread(file_path_str)
-    # Binarize npf-coated bead surface using Canny edge detector.
-    npf_mat = mm_stack[0, :, :]
-    npf_rescaled_mat = rescale_intensity(1.0 * npf_mat)
-    bw_mat = binary_fill_holes(canny(npf_rescaled_mat, sigma = 0.2))
-    bw_mat = remove_small_objects(bw_mat, min_size = sum(disk(6)))
-    bw_mat ^= erosion(bw_mat, disk(3))
-    # Measure npf intensity.
-    label_mat, no_labels = label(bw_mat, return_num = True)
-    mean_background_int = mean(npf_mat[~bw_mat])
-    properties_list = regionprops(label_mat, npf_mat - mean_background_int)
-    diameter_row = 0.5 * MICRON_PER_PIXEL * array([x.major_axis_length + x.minor_axis_length for x in properties_list])
-    is_large_row = diameter_row > DIAMETER_THRESHOLD
-    npf_intensity_row = array([x.mean_intensity for x in properties_list])
-    # Save images.
-    if save_images == True:
-        npf_rgb_mat = make_rgb_overlay(npf_rescaled_mat, bw_mat, [0.0, 1.0, 0.0])
-        imsave(file_path_str[:-4] + '_npf_segmentation.tif', img_as_uint(npf_rgb_mat))
-    return npf_intensity_row
-    
-def measure_actin_density(file_path_str, save_images = False):
-    # Load micromanager stack.
-    mm_stack = imread(file_path_str)
-    # Binarize npf-coated bead surface using Canny edge detector.
-    npf_mat = mm_stack[0, :, :]
-    npf_rescaled_mat = rescale_intensity(1.0 * npf_mat)
-    bw_mat = binary_fill_holes(canny(npf_rescaled_mat, sigma = 0.3))
-    bw_mat = remove_small_objects(bw_mat, min_size = sum(disk(6)))
-    bw_mat ^= erosion(bw_mat, disk(3))
-    # Measure npf intensity.
-    label_mat, no_labels = label(bw_mat, return_num = True)
-    mean_background_int = mean(npf_mat[~bw_mat])
-    properties_list = regionprops(label_mat, npf_mat - mean_background_int)
-    diameter_row = 0.5 * MICRON_PER_PIXEL * array([x.major_axis_length + x.minor_axis_length for x in properties_list])
-    is_large_row = diameter_row > DIAMETER_THRESHOLD
-    npf_intensity_row = array([x.mean_intensity for x in properties_list])
-    # Save images.
-    if save_images == True:
-        npf_rgb_mat = make_rgb_overlay(npf_rescaled_mat, bw_mat, [0.0, 1.0, 0.0])
-        imsave(file_path_str[:-4] + '_npf_segmentation.tif', img_as_uint(npf_rgb_mat))
-    
-    # Measure actin intensity.
-    actin_intensity_row = zeros(npf_intensity_row.shape)
-    if no_labels >= 1:
-        actin_mat = gaussian(mm_stack[1, :, :], sigma = 0.5)
-        actin_rescaled_mat = rescale_intensity(1.0 * mm_stack[1, :, :])
-        mean_actin_back_au = mean(actin_mat)
-        std_actin_back_au = std(actin_mat)
-        bw_otsu_mat = actin_mat > max(threshold_otsu(actin_mat),
-                                      mean_actin_back_au + 3 * std_actin_back_au)
-        bw_otsu_mat = remove_small_objects(bw_otsu_mat, min_size = sum(disk(3)))
-        bw_actin_mat = zeros(actin_mat.shape, dtype = bool)
-        for i in range(1, no_labels + 1):
-            i_bw_npf_mat = binary_fill_holes(label_mat == i)
-            i_bw_dilated_mat = dilation(i_bw_npf_mat, disk(15))
-            i_bw_search_mat = i_bw_dilated_mat ^ i_bw_npf_mat
-            i_bw_actin_mat = i_bw_search_mat & bw_otsu_mat
-            if all(i_bw_actin_mat == False):
-                actin_intensity_row[i - 1] = mean_actin_back_au
-            else:
-                actin_intensity_row[i - 1] = mean(actin_mat[i_bw_actin_mat]) - mean_actin_back_au
-                bw_actin_mat[i_bw_search_mat & bw_otsu_mat] = True
-        if save_images == True:
-            actin_rgb_mat = make_rgb_overlay(actin_rescaled_mat, bw_actin_mat, [1.0, 0.0, 0.0])
-            imsave(file_path_str[:-4] + '_actin_segmentation.tif', img_as_uint(actin_rgb_mat))
-    return npf_intensity_row, actin_intensity_row
-        
-def batch_measure_actin_density(folder_name_str, save_images = False):
-    file_path_list = find_tif_files(folder_name_str)
-    no_files = len(file_path_list)
-    npf_intensity_row = array([])
-    actin_intensity_row = array([])
-    for i in range(no_files):
-        i_intensity_row, i_actin_intensity_row = measure_actin_density(file_path_list[i], save_images)
-        npf_intensity_row = append(npf_intensity_row, i_intensity_row)
-        actin_intensity_row = append(actin_intensity_row, i_actin_intensity_row)
-    return npf_intensity_row, actin_intensity_row
+def save_npf_segmentation(file_path_str, npf_im, npf_bw_im):
+    npf_rgb_im = make_rgb_overlay(npf_im, npf_bw_im, [0.0, 1.0, 0.0])
+    imsave(file_path_str[:-4] + '_npf_segmentation.tif', img_as_uint(npf_rgb_im))
 
-def batch_measure_npf_density(folder_name_str, save_images = False):
-    file_path_list = find_tif_files(folder_name_str)
+def save_segmentation(file_path_str, actin_im, npf_bw_im, tail_axis_bw_im):
+    rgb_im = make_rgb_overlay(actin_im, npf_bw_im | tail_axis_bw_im, [0.0, 1.0, 0.0])
+    imsave(file_path_str[:-4] + '_tail_segmentation.tif', img_as_uint(rgb_im))
+
+def binarize_actin(actin_im):
+    actin_blurred_im = gaussian(actin_im, sigma = 2)
+    threshold1 = threshold_otsu(actin_blurred_im)
+    actin_bw1_im = actin_blurred_im > threshold1
+    back_mean_fluor = mean(actin_blurred_im[~actin_bw1_im].flatten())
+    back_std_fluor = std(actin_blurred_im[~actin_bw1_im].flatten())
+    threshold2 = back_mean_fluor + 3 * back_std_fluor
+    threshold3 = max([threshold1, threshold2])
+    actin_bw2_im = actin_blurred_im > threshold3
+    return actin_bw2_im
+
+def save_actin_segmentation(file_path_str, actin_im, actin_bw_im):
+    actin_rgb_im = make_rgb_overlay(actin_im, actin_bw_im, [1.0, 0.0, 0.0])
+    imsave(file_path_str[:-4] + '_actin_segmentation.tif', img_as_uint(actin_rgb_im))
+
+def measure_comet_props(npf_im, npf_bw_im, actin_im, actin_bw_im):
+    npf_label_im, no_beads = label(npf_bw_im, return_num = True)
+    npf_back_mean = mean(npf_im[~npf_bw_im].flatten())
+    actin_back_mean = mean(actin_im[~actin_bw_im].flatten())
+    comet_props_mat = empty((1, 3))
+    tail_axis_bw_im = zeros(actin_im.shape, dtype = bool)
+    for i in range(1, no_beads + 1):
+        i_bead_bw_im = npf_label_im == i
+        i_npf_fluor = mean(npf_im[i_bead_bw_im].flatten()) - npf_back_mean
+        i_npf_fluor *= (2**16 - 1)
+        i_bead_actin_bw_im = i_bead_bw_im & actin_bw_im
+        i_tail_bw_im = reconstruction(i_bead_actin_bw_im, actin_bw_im)
+        i_tail_axis_bw_im = medial_axis(i_tail_bw_im)
+        tail_axis_bw_im[i_tail_axis_bw_im] = True
+        i_tail_fluor = mean(actin_im[i_tail_axis_bw_im].flatten()) - actin_back_mean
+        i_tail_fluor *= (2**16 - 1)
+        i_tail_length = sum(i_tail_axis_bw_im.flatten()) * MICRON_PER_PIXEL
+        comet_props_mat = vstack((comet_props_mat, [[i_npf_fluor, i_tail_fluor, i_tail_length]]))
+    return comet_props_mat[1:], tail_axis_bw_im
+
+def batch_analysis(folder_path_str):
+    file_path_list = find_tif_files(folder_path_str)
     no_files = len(file_path_list)
-    npf_intensity_row = array([])
+    comet_props_mat = empty((1, 3))
     for i in range(no_files):
-        i_intensity_row = measure_npf_density(file_path_list[i], save_images)
-        npf_intensity_row = append(npf_intensity_row, i_intensity_row)
-    return npf_intensity_row
+        i_npf_im, i_actin_im = read_and_split(file_path_list[i])
+        i_npf_bw_im = binarize_npf(i_npf_im)
+        i_actin_bw_im = binarize_actin(i_actin_im)
+        i_actin_label_im, i_no_labels = label(i_actin_bw_im, return_num = True)
+        if i_no_labels > 0:
+            i_comet_props_mat, i_tail_axis_bw_im = measure_comet_props(i_npf_im, i_npf_bw_im, i_actin_im, i_actin_bw_im)
+            comet_props_mat = vstack((comet_props_mat, i_comet_props_mat))
+            save_segmentation(file_path_list[i], i_actin_im, i_npf_bw_im, i_tail_axis_bw_im)
+    return comet_props_mat[1:]
