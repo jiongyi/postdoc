497e262ef0a838de1bfad896ea471e7872209190
Jiongyi Tan <jiongyi.tan@ucsf.edu>
2020-05-17 10:55:34 -0700

    worked on phase diagrams

--- actin_assembly/simulation/mueller_sixt/.ipynb_checkpoints/mueller_sixt-checkpoint.py
+++ actin_assembly/simulation/mueller_sixt/.ipynb_checkpoints/mueller_sixt-checkpoint.py
@@ -8,7 +8,7 @@ def numba_nonzero(row):
     return row.nonzero()
 
 class Network(object):
-    def __init__(self, branching_rate_const = 1.0, capping_rate_const = 1.0, total_time = 20.0):
+    def __init__(self, branching_rate_const = 0.5, capping_rate_const = 1.0, total_time = 20.0):
         # Copy argument values.
         self.elongation_rate_const = 141.0 # in nm/ms
         self.branching_rate_const = branching_rate_const
@@ -18,7 +18,7 @@ class Network(object):
         
         # Define constants.
         self.monomer_width = 2.7 # in nm
-        self.branching_region_width = 5 * self.monomer_width
+        self.branching_region_width = 10 * self.monomer_width
         
         # Initialize variables.
         self.pointed_position_mat = zeros((200, 2))
@@ -56,19 +56,10 @@ class Network(object):
         
     def cap(self, filament_index):
         self.is_capped_row[filament_index] = True
-        
-    def compute_transition_rates(self):
-        self.transition_rate_mat = zeros((self.barbed_position_mat.shape[0], 3))
-        # Elongation.
-        self.transition_rate_mat[~self.active_in_region_index, 0] = self.elongation_rate_const * exp(-0.3 * self.monomer_width / 4.114 / self.active_in_region_index.size * 1e3)
-        # Branching is zeroth-order w.r.t. filament count.
-        self.transition_rate_mat[self.active_in_region_index, 1] = self.branching_rate_const
-        # Capping is first-order w.r.t. filament count.
-        self.transition_rate_mat[self.active_in_region_index, 2] = self.capping_rate_const # First order
-        
+                
     def catalog_filaments(self):
         self.active_in_region_index = logical_and(self.leading_edge_position - self.barbed_position_mat[:, 0] <= self.branching_region_width, ~self.is_capped_row).nonzero()[0]
-        
+                
     def metropolis_step(self):
         for index in self.active_in_region_index:
             # Capping
@@ -82,37 +73,16 @@ class Network(object):
             if poisson(self.elongation_rate_const * self.time_interval) == True:
                 self.elongate(index)
         self.current_time += self.time_interval
-        self.leading_edge_position += self.elongation_rate_const * self.time_interval
-            
-        
-    def gillespie_step(self):
-        nonzero_transition_row_mat, nonzero_transition_col_mat = numba_nonzero(self.transition_rate_mat)
-        nonzero_transition_rate_row = self.transition_rate_mat[nonzero_transition_row_mat, nonzero_transition_col_mat].flatten()
-        random_mat = rand(nonzero_transition_rate_row.size)
-        time_mat = -log(random_mat) / nonzero_transition_rate_row
-        random_transition_index = time_mat.argmin()
-        time_increment = time_mat[random_transition_index]
-        transition_row = nonzero_transition_row_mat[random_transition_index]
-        transition_col = nonzero_transition_col_mat[random_transition_index]
-        if transition_col == 0:
-            self.elongate(transition_row)
-            self.leading_edge_position = self.barbed_position_mat[:, 0].max()
-        elif transition_col == 1:
-            self.branch(transition_row)
-        elif transition_col == 2:
-            self.cap(transition_row)
-        self.current_time += time_increment
-                
+        self.leading_edge_position += self.elongation_rate_const * self.monomer_width * self.time_interval
+                            
     def simulate(self):
         self.catalog_filaments()
         while logical_and(self.current_time <= self.total_time, self.active_in_region_index.size != 0):
-            self.compute_transition_rates()
             self.metropolis_step()
-            #self.gillespie_step()
             self.catalog_filaments()
             
     def compute_order_parameter(self):
-        orientation_degree_row = 180 * self.filament_orientation_row[self.active_in_region_index] / pi
+        orientation_degree_row = 180 * self.filament_orientation_row[200:] / pi
         zero_bin_count = (abs(orientation_degree_row) <= 17.5).sum()
         thirtyfive_bin_count = 0.5 * ((abs(orientation_degree_row - 35.0) <= 17.5).sum() + (abs(orientation_degree_row + 35.0) <= 17.5).sum())
         self.order_parameter = (zero_bin_count - thirtyfive_bin_count) / (zero_bin_count + thirtyfive_bin_count)
@@ -133,7 +103,7 @@ class Network(object):
     
     def plot_orientation(self):
         fig_hand, axes_hand = subplots()
-        axes_hand.hist(180 * self.filament_orientation_row[self.active_in_region_index] / pi, [-90, -87.5, -52.5, -17.5, 17.5, 52.5, 87.5, 90])
+        axes_hand.hist(180 * self.filament_orientation_row[200:] / pi, [-90, -87.5, -52.5, -17.5, 17.5, 52.5, 87.5, 90])
         return fig_hand, axes_hand
 
         
