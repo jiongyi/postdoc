8b6a46641696dcdd8edfd22b0364581ee01f69e2
Jiongyi Tan <jiongyi.tan@ucsf.edu>
2020-09-22 11:49:41 -0700

    Updated bead motility code

--- actin_assembly/bead_motility/bead_motility.py
+++ actin_assembly/bead_motility/bead_motility.py
@@ -1,16 +1,15 @@
 from os import walk
-from os.path import join, split
+from os.path import join
 import fnmatch
-from skimage import img_as_float, img_as_uint, dtype_limits
+from skimage import img_as_float, img_as_ubyte
 from skimage.io import imread, imsave
-from skimage.feature import canny
 from scipy.ndimage.morphology import binary_fill_holes
-from skimage.morphology import disk, dilation, erosion, remove_small_objects, reconstruction, skeletonize, convex_hull_image
-from skimage.filters import threshold_otsu, gaussian
-from numpy import sum, mean, median, std, array, empty, vstack, zeros, stack
-from numpy.random import randint
+from skimage.morphology import closing, disk, erosion, opening, reconstruction, remove_small_objects, skeletonize
+from skimage.filters import threshold_otsu
+from numpy import array, mean, nan, stack, sum, zeros
 from skimage.measure import label, regionprops
 from skimage.segmentation import find_boundaries, clear_border
+from pandas import DataFrame, concat
 
 MICRON_PER_PIXEL = 0.16
 DIAMETER_THRESHOLD = 2.0
@@ -24,82 +23,111 @@ def find_mmstack_files(folder_name_str):
                 mmstack_file_path_list.append(join(dir_name_str, i_file_name))
     return mmstack_file_path_list
 
-def read_and_split(file_path_str):
+def load_stack(file_path_str):
     mm_stack = imread(file_path_str)
-    npf_im = img_as_float(mm_stack[0, :, :])
-    actin_im = img_as_float(mm_stack[1, :, :])
-    return npf_im, actin_im
+    npf_im = img_as_float(mm_stack[:, :, 0])
+    pulse_im = img_as_float(mm_stack[:, :, 1])
+    chase_im = img_as_float(mm_stack[:, :, 2])
+    return npf_im, pulse_im, chase_im
+    
+def measure_tail_length(mm_stack):
+    npf_im = img_as_float(mm_stack[:, :, 0])
+    pulse_im = img_as_float(mm_stack[:, :, 1])
+    chase_im = img_as_float(mm_stack[:, :, 2])
+    snr_npf_im = compute_snr(npf_im)
+    snr_pulse_im = compute_snr(pulse_im)
+    snr_chase_im = compute_snr(chase_im)
+    bw_chase_im = snr_chase_im > threshold_otsu(snr_chase_im)
+    snr_composite_im = (snr_npf_im + snr_pulse_im + snr_chase_im) / 3
+    snr_composite_im = opening(snr_composite_im, disk(6))
+    bw_composite_im = snr_composite_im > threshold_otsu(snr_composite_im)
+    skel_composite_im = skeletonize(bw_composite_im, method = 'lee')
+    bw_tail_im = skel_composite_im & bw_chase_im
+    tail_props_list = regionprops(label(bw_tail_im))
+    if len(tail_props_list) > 0:
+        tail_length = tail_props_list[0].area * MICRON_PER_PIXEL
+        return tail_length
+    else:
+        return nan
 
-def binarize_npf(npf_im):
-    npf_rescaled_im = npf_im / max(npf_im.flatten())
-    bw_im = binary_fill_holes(canny(npf_rescaled_im, sigma = 0.2))
-    bw_im = remove_small_objects(bw_im, min_size = sum(disk(6)))
-    npf_bw_im = clear_border(bw_im ^ erosion(bw_im, disk(3)))
+def save_segmentation(file_path_str, gray_im, bw_stack):
+    gray_im /= gray_im.max()
+    rgb_stack = stack((gray_im, gray_im, gray_im), axis = -1)
+    bw_stack[:, :, 1] = find_boundaries(bw_stack[:, :, 1])
+    bw_stack[:, :, 2] = find_boundaries(bw_stack[:, :, 2])
+    rgb_stack[bw_stack[:, :, 1], 0] = 1
+    rgb_stack[bw_stack[:, :, 1], 1] = 0
+    rgb_stack[bw_stack[:, :, 1], 2] = 0
+    rgb_stack[bw_stack[:, :, 2], 0] = 0
+    rgb_stack[bw_stack[:, :, 2], 1] = 1
+    rgb_stack[bw_stack[:, :, 2], 2] = 0
+    rgb_stack[bw_stack[:, :, 3], 0] = 0
+    rgb_stack[bw_stack[:, :, 3], 1] = 0
+    rgb_stack[bw_stack[:, :, 3], 2] = 1
+    imsave(file_path_str[:-21] + '_segmentation.jpg', img_as_ubyte(rgb_stack))
+    
+def segment_npf(npf_im):
+    npf_closed_im = closing(npf_im, disk(3))
+    npf_opened_im = opening(npf_closed_im, disk(3))
+    npf_bw_im = npf_opened_im > threshold_otsu(npf_opened_im)
+    npf_bw_im = remove_small_objects(npf_bw_im, min_size = sum(disk(6)))
+    npf_bw_im = clear_border(npf_bw_im ^ erosion(npf_bw_im, disk(3)))
     return npf_bw_im
 
-def make_rgb_overlay(gray_im, bw_im, rgb_color_row):
-    gray_im = gray_im / max(gray_im.flatten())
-    max_type = dtype_limits(gray_im)[1]
-    red_scale = rgb_color_row[0]
-    green_scale = rgb_color_row[1]
-    blue_scale = rgb_color_row[2]
-    rgb_im = stack((gray_im, gray_im, gray_im), axis = -1)
-    rgb_im[bw_im, 0] = red_scale * max_type
-    rgb_im[bw_im, 1] = green_scale * max_type
-    rgb_im[bw_im, 2] = blue_scale * max_type
-    return rgb_im
-
-def save_segmentation(file_path_str, actin_im, npf_bw_im):
-    rgb_im = make_rgb_overlay(actin_im, npf_bw_im, [0.0, 1.0, 0.0])
-    imsave(file_path_str[:-21] + '_npf_segmentation.tif', img_as_uint(rgb_im))
-
-def binarize_actin(actin_im):
-    actin_blurred_im = gaussian(actin_im, sigma = 2)
-    threshold1 = threshold_otsu(actin_blurred_im)
-    actin_bw1_im = actin_blurred_im > threshold1
-    back_mean_fluor = mean(actin_blurred_im[~actin_bw1_im].flatten())
-    back_std_fluor = std(actin_blurred_im[~actin_bw1_im].flatten())
-    threshold2 = back_mean_fluor + 3 * back_std_fluor
-    threshold3 = max([threshold1, threshold2])
-    actin_bw2_im = clear_border(actin_blurred_im > threshold3)
-    actin_bw3_im = binary_fill_holes(actin_bw2_im)
-    return actin_bw3_im
-
-def measure_comet_props(npf_im, npf_bw_im, actin_im):
-    npf_label_im, no_beads = label(npf_bw_im, return_num = True)
-    npf_back_mean = mean(npf_im[~binary_fill_holes(npf_bw_im)].flatten())
-    actin_back_mean = mean(actin_im[~binary_fill_holes(npf_bw_im)].flatten())
-    comet_props_mat = empty((1, 2))
-    for i in range(1, no_beads + 1):
-        i_bead_bw_im = npf_label_im == i
-        i_npf_fluor = mean(npf_im[i_bead_bw_im].flatten()) - npf_back_mean
-        i_npf_fluor *= (2**16 - 1)
-        i_actin_fluor = mean(actin_im[i_bead_bw_im].flatten()) - actin_back_mean
-        i_actin_fluor *= (2**16 - 1)
-        comet_props_mat = vstack((comet_props_mat, [[i_npf_fluor, i_actin_fluor]]))
-    return comet_props_mat[1:]
-
-def batch_analysis(folder_path_str):
-    mmstack_file_path_list = find_mmstack_files(folder_path_str)
-    no_files = len(mmstack_file_path_list)
-    comet_props_mat = empty((1, 2))
-    for i in range(no_files):
-        print(mmstack_file_path_list[i])
-        i_npf_im, i_actin_im = read_and_split(mmstack_file_path_list[i])
-        i_npf_bw_im = binarize_npf(i_npf_im)
-        i_npf_label_im, i_no_labels = label(i_npf_bw_im, return_num = True)
-        if i_no_labels > 0:
-            i_comet_props_mat = measure_comet_props(i_npf_im, i_npf_bw_im, i_actin_im)
-            comet_props_mat = vstack((comet_props_mat, i_comet_props_mat))
-            save_segmentation(mmstack_file_path_list[i], i_npf_im, i_npf_bw_im)
-    return comet_props_mat[1:]
-
-def display_sample(folder_path_str, no_samples):
-    mmstack_file_path_list = find_mmstack_files(folder_path_str)
-    no_files = len(mmstack_file_path_list)
-    samples_index = randint(0, no_files, no_samples)
-    display_im = imread(mmstack_file_path_list[samples_index[0]])[1, :, :]
-    for i in range(1, no_samples):
-        display_im = vstack((display_im, imread(mmstack_file_path_list[samples_index[i]])[1, :, :]))
-    return display_im
+def segment_actin(actin_im):
+    actin_closed_im = closing(actin_im, disk(3))
+    actin_opened_im = opening(actin_closed_im, disk(3))
+    actin_bw_im = clear_border(actin_opened_im > (actin_opened_im.mean() + 3 * actin_opened_im.std()))
+    return binary_fill_holes(actin_bw_im)
     
+def measure_comet_tail_props(npf_im, pulse_im, chase_im):
+    npf_bw_im = segment_npf(npf_im)
+    pulse_bw_im = segment_actin(pulse_im)
+    chase_bw_im = segment_actin(chase_im)
+    npf_back_mean = mean(npf_im[~npf_bw_im])
+    pulse_back_mean = mean(pulse_im[~pulse_bw_im])
+    chase_back_mean = mean(chase_im[~chase_bw_im])
+    composite_bw_im = binary_fill_holes(npf_bw_im | pulse_bw_im | chase_bw_im)
+    npf_label_im, no_beads = label(npf_bw_im, return_num = True)
+    npf_fluor_row = zeros(no_beads)
+    actin_fluor_row = zeros(no_beads)
+    tail_length_row = zeros(no_beads)
+    tail_bw_im = zeros(npf_im.shape, dtype = bool)
+    for i in range(no_beads):
+        i_npf_bw_im = (npf_bw_im == (i + 1))
+        npf_fluor_row[i] = mean(npf_im[i_npf_bw_im]) - npf_back_mean
+        i_pulse_bw_im = reconstruction(i_npf_bw_im & pulse_bw_im, pulse_bw_im).astype(bool)
+        i_chase_bw_im = reconstruction(i_npf_bw_im & chase_bw_im, chase_bw_im).astype(bool)
+        i_composite_bw_im = reconstruction(i_npf_bw_im & composite_bw_im, composite_bw_im).astype(bool)
+        i_composite_skel_im = skeletonize(erosion(i_composite_bw_im, disk(3)), method = 'lee')
+        if i_pulse_bw_im.sum() > i_chase_bw_im.sum():
+            i_tail_bw_im = i_composite_skel_im & i_chase_bw_im
+            actin_fluor_row[i] = mean(chase_im[i_chase_bw_im]) - chase_back_mean
+        else:
+            i_tail_bw_im = i_composite_skel_im & i_pulse_bw_im
+            actin_fluor_row[i] = mean(pulse_im[i_pulse_bw_im]) - pulse_back_mean
+        i_tail_props_list = regionprops(label(i_tail_bw_im))
+        if len(i_tail_props_list) == 0:
+            tail_length_row[i] = nan
+        else:
+            tail_length_row[i] = i_tail_props_list[0].area * MICRON_PER_PIXEL
+            tail_bw_im[i_tail_bw_im == True] = True
+    comet_tail_props_df = DataFrame.from_dict({'npf_fluor': npf_fluor_row * (2**16 - 1),
+                                               'actin_fluor': actin_fluor_row * (2**16 -1),
+                                               'tail_length': tail_length_row})
+    bw_stack = stack((npf_bw_im, pulse_bw_im, chase_bw_im, tail_bw_im), axis = -1)
+    return comet_tail_props_df, bw_stack
+    
+def batch_analysis(folder_path_str):
+    comet_tail_props_df = DataFrame({'npf_fluor': array([]), 'actin_fluor': array([]), 'tail_length': array([])})
+    mmstack_file_path_list = find_mmstack_files(folder_path_str)
+    no_files = len(mmstack_file_path_list)
+    for i in range(no_files):
+        i_npf_im, i_pulse_im, i_chase_im = load_stack(mmstack_file_path_list[i])
+        i_comet_tail_props_df, i_bw_stack = measure_comet_tail_props(i_npf_im, i_pulse_im, i_chase_im)
+        i_comet_tail_props_df['file_path'] = mmstack_file_path_list[i]
+        comet_tail_props_df = concat([comet_tail_props_df, i_comet_tail_props_df])
+        save_segmentation(mmstack_file_path_list[i], i_chase_im, i_bw_stack)
+    comet_tail_props_df = comet_tail_props_df.reset_index()
+    comet_tail_props_df.to_csv('comet_tail_properties.csv')
+    return comet_tail_props_df
